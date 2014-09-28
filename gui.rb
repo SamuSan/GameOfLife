f2bb63b01a4138062d13f17ac99d07313b492d2f
SamuSan <sambminns@gmail.com>
2014-09-29 10:38:07 +1300

    Starting with GUI, Tk is a pig, thinking of switching this to a rails browser app

---
+++ gui.rb
@@ -0,0 +1,15 @@
+class Gui
+
+  #Need to give the gui god and set it running
+  #
+
+
+
+
+
+
+
+
+
+
+end
--- main.rb
+++ main.rb
@@ -2,10 +2,13 @@ require_relative 'god'
 require_relative 'grub'
 require 'tk'
 
-
-$god = God.new()
-while $god.life do
-  $god.print_grubs
-  $god.evolve
-end
+# root = TkRoot.new()
+#
+# @canvas = TkCanvas.new(root)
+# $god = God.new()
+# while $god.life do
+#   $god.print_grubs
+#   $god.evolve
+#   Tk.mainloop
+# end
 
